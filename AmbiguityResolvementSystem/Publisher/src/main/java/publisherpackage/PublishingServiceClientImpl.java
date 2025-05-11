6713b3f46ec428d84bdd884b941c2d03bec6e287
sejmasijaric <sejma.sijaric@student.unisg.ch>
2025-05-11 10:46:39 +0200

    publishermodule -> configurations decouples, whole pipeline working

--- AmbiguityResolvementSystem/Publisher/src/main/java/publisherpackage/PublishingServiceClientImpl.java
+++ AmbiguityResolvementSystem/Publisher/src/main/java/publisherpackage/PublishingServiceClientImpl.java
@@ -1,5 +1,6 @@
 package publisherpackage;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -8,72 +9,28 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
 import java.util.Map;
 
 @Service
 public class PublishingServiceClientImpl implements PublishingServiceClient {
-    private final String broker = "tcp://broker.emqx.io:1883";
+    private static final Logger logger = LoggerFactory.getLogger(PublishingServiceClientImpl.class);
 
-    @Override
-    public void publishResolvedAmbiguousEvent(String mlOutput, String ambiguousJsonEvents) throws MqttException {
-        try {
-            // read the ml output --> check if ambiguity got resolved
-            ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode rootMLOutput = objectMapper.readTree(mlOutput);
-            JsonNode results = rootMLOutput.path("results");
-            String topClass = results.path("top_class").asText();
-            boolean resolvedAmb = results.path("resolved_ambiguity").asBoolean();
-
-            // get the list of ambiguous events
-            JsonNode eventsRoot = objectMapper.readTree(ambiguousJsonEvents);
-            ArrayNode ambiguousEvents = (ArrayNode) eventsRoot.get("events");
-
-            if (resolvedAmb && !ambiguousEvents.isEmpty()) {
-                ObjectNode resolvedEvent = objectMapper.createObjectNode();
-                resolvedEvent.put("concept:name", topClass);
-                // get the data that's same for all the ambiguous events
-                JsonNode firstEvent = ambiguousEvents.get(0);
-                for(int i = 1 ; i < firstEvent.size(); i++) {
-                    String key = firstEvent.get(i).asText();
-                    String value = firstEvent.get(key).asText();
-                    resolvedEvent.put(key, value);
-                }
-                // Resource: https://docs.oracle.com/cd/B14099_12/web.1012/b12024/javax/xml/parsers/DocumentBuilderFactory.html#:~:text=Class%20DocumentBuilderFactory&text=Defines%20a%20factory%20API%20that,object%20trees%20from%20XML%20documents.&text=Allows%20the%20user%20to%20retrieve%20specific%20attributes%20on%20the%20underlying%20implementation.
-                // converting json to xml
-                // create a factory for XML document builders
-                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                // Use the factory to create a document builder
-                DocumentBuilder builder = factory.newDocumentBuilder();
-                // create a new, empty XML document in memory
-                Document doc = builder.newDocument();
-                String resolvedEventString = jsonToXesMapper(resolvedEvent, doc);
-                publishMessage( "clean-events", resolvedEventString);
-            } else {
-                System.out.println("Ambiguity not resolved. Manual intervention needed on following events:");
-                System.out.println("Confidence for classes: " + results.path("class_confidences").toString());
-                for (JsonNode event : ambiguousEvents) {
-                    System.out.println(event.toString());
-                }
-                System.out.println("Events published to topic: ambiguous-events\nPlease subscribe to the topic to see the events.");
-                System.out.println("Frame paths relevant for ambiguity resolution are: " + results.path("frames_paths").toString());
-                publishMessage("ambiguous-events", ambiguousJsonEvents);
-            }
-        } catch (Exception e) {
-            System.err.println("Error processing ML output: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
+    // load configuration
+    ConfigLoader config = new ConfigLoader();
+    private final String topicUnambiguousEvents = config.get("publisher.topicUnambiguousEvent");
+    private final String topicAmbiguousEvents = config.get("publisher.topicAmbiguousEvent");
 
     private MqttClient connectToBroker() throws MqttException {
-        System.out.println("Connecting to broker: " + broker);
+        String broker = "tcp://broker.emqx.io:1883";
+        logger.info("Connecting to broker: " + broker);
         MqttClient client = new MqttClient(broker, MqttClient.generateClientId());
         MqttConnectOptions options = new MqttConnectOptions();
         options.setCleanSession(true);
@@ -81,101 +38,142 @@ public class PublishingServiceClientImpl implements PublishingServiceClient {
         try {
             client.connect(options);
         } catch (MqttException e) {
-            System.err.println("MQTT connection failed: " + e.getReasonCode());
-            e.printStackTrace();
-            throw e;
-        }        System.out.println("Connected to broker: " + broker);
+            logger.error("MQTT connection failed: " + e.getReasonCode());
+        }
         return client;
     }
 
-    private String jsonToXesMapper(JsonNode jsonEvent, Document doc) {
-        Element eventElement = doc.createElement("event");
+    /**
+     * Checks if the ambiguity got resolved by the ML model
+     * If yes: Constructs the xml event and publishes it to the MQTT broker
+     * If no: Publishes the ambiguous events to the MQTT broker on a different topic
+     *
+     * @param mlOutput ML output from the ML model
+     * @param ambiguousJsonEvents JSON string containing ambiguous events
+     */
+    @Override
+    public void publishResolvedAmbiguousEvent(String mlOutput, String ambiguousJsonEvents) throws MqttException {
 
-        Iterator<Map.Entry<String, JsonNode>> fields = jsonEvent.fields();
-        while (fields.hasNext()) {
-            Map.Entry<String, JsonNode> entry = fields.next();
-            String key = entry.getKey();
-            if (entry.getValue().isTextual()) {
-                Element stringElement = doc.createElement("string");
-                stringElement.setAttribute("key", key);
-                stringElement.setAttribute("value", entry.getValue().asText());
-                eventElement.appendChild(stringElement);
+        try {
+            // read the ml output --> get necessary data
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootMLOutput = objectMapper.readTree(mlOutput);
+            String topClass = rootMLOutput.path("top_class").asText();
+            boolean resolvedAmb = rootMLOutput.path("resolved_ambiguity").asBoolean();
+            String classConfidences = rootMLOutput.path("class_confidences").toString();
+            String framePaths = rootMLOutput.path("frames_paths").toString();
+
+            // get the list of ambiguous events
+            JsonNode eventsRoot = objectMapper.readTree(ambiguousJsonEvents);
+            ArrayNode ambiguousEvents = (ArrayNode) eventsRoot.get("events");
+
+            // check if ambiguity got resolved by ml model
+            if (resolvedAmb) {
+                logger.info("Ambiguity resolved. Top class: " + topClass);
+                // create new JSON object for the resolved event
+                ObjectNode resolvedEvent = objectMapper.createObjectNode();
+                resolvedEvent.put("concept:name", topClass);
+
+                // get the data that is same for all the ambiguous events
+                JsonNode firstEvent = ambiguousEvents.get(0);
+                Iterator<Map.Entry<String, JsonNode>> fields = firstEvent.fields();
+                while (fields.hasNext()) {
+                    Map.Entry<String, JsonNode> entry = fields.next();
+                    String key = entry.getKey();
+                    if (key.equals("concept:name")) {
+                        continue; // skip this key
+                    }
+                    String value = entry.getValue().asText();
+                    resolvedEvent.put(key, value);
+                }
+                // convert the new JSON event to XES format
+                JsonToXesMapper jsonToXesMapper = new JsonToXesMapper();
+                String resolvedEventString = jsonToXesMapper.convertJsonToXes(resolvedEvent.toString());
+                logger.info("Publishing event with resolved ambiguity: " + resolvedEventString);
+                publishEvent(topicUnambiguousEvents, resolvedEventString);
             } else {
-                Element dateElement = doc.createElement("date");
-                dateElement.setAttribute("key", key);
-                dateElement.setAttribute("value", entry.getValue().asText());
-                eventElement.appendChild(dateElement);
+                // handle case when ambiguity is not resolved (the classification confidence is below the threshold)
+                logger.error("Confidence for classes: " + classConfidences);
+                logger.error("Ambiguity not resolved. Manual intervention needed on following events:");
+                for (JsonNode event : ambiguousEvents) {
+                    logger.info("Event: " + event.toString());
+                }
+                logger.info("Events published to topic: ambiguous-events. Please subscribe to the topic to see the events.");
+                logger.info("Frame paths relevant for manual ambiguity resolution are: " + framePaths);
+                publishEvent(topicAmbiguousEvents, ambiguousJsonEvents);
             }
+        } catch (Exception e) {
+            logger.error("Error processing ML output: " + e.getMessage());
+            e.printStackTrace();
         }
-        // prepare the event format here
-        return eventElement.toString();
     }
 
+    /**
+     * Constructs the xml event from the JSON string and publishes it to the MQTT broker
+     *
+     * @param unambEvent the unambiguous event in JSON format
+     */
     @Override
     public void publishUnambiguousEvent(String unambEvent) throws MqttException {
-        System.out.println("Publishing unambiguous event...");
-        String topic = "clean-events";
-        // create a factory for XML document builders
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        // Use the factory to create a document builder
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        // create a new, empty XML document in memory
-        Document doc = builder.newDocument();
-        String json_event = jsonToXesMapper(unambEvent, doc)
-        publishMessage(topic, json_event);
-        System.out.println("Unambiguous event published to topic: " + topic);
+
+        try {
+            // get the unambiguous event
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(unambEvent);
+            JsonNode eventsNode = root.get("events");
+            String eventsAsString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(eventsNode);
+
+            // convert the JSON event to XES format
+            JsonToXesMapper jsonToXesMapper = new JsonToXesMapper();
+            String xesEvent = jsonToXesMapper.convertJsonToXes(eventsAsString);
+            logger.info("Publishing unambiguous event: " + xesEvent);
+            publishEvent(topicUnambiguousEvents, xesEvent);
+        } catch (JsonProcessingException err) {
+            logger.error("Error processing JSON event: " + err.getMessage());
+            err.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            logger.error("Error creating XML document: " + e.getMessage());
+            throw new RuntimeException(e);
+        } catch (TransformerException e) {
+            logger.error("Error transforming XML document: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
-    public void publishMessage (String topic, String message) throws MqttException {
+    /**
+     * Connect to broker and publish event to the MQTT broker
+     *
+     * @param topic the topic to publish to
+     * @param message the message to publish
+     */
+    public void publishEvent (String topic, String message) throws MqttException {
+        // connect to the broker
         MqttClient client = connectToBroker();
-        // parse correctly to prepare the message in the initial format
         try {
+            // check connection to broker and publish message
             if (client.isConnected()) {
-                System.out.println("Client is connected to the broker.");
+                logger.info("Client is connected to the broker.");
                 MqttMessage mqttMessage = new MqttMessage(message.getBytes(StandardCharsets.UTF_8));
                 mqttMessage.setQos(1);
                 client.publish(topic, mqttMessage);
-                System.out.println("Message published: " + message);
+                logger.info("Event published successfully to topic: " + topic);
             } else {
-                System.out.println("Client is not connected to the broker.");
+                logger.error("Client is not connected to the broker.");
             }
         } catch (MqttException e) {
+            logger.error("Error while publishing message: " + e.getMessage());
             e.printStackTrace();
+        // disconnect from broker after sending event
         } finally {
             try {
                 if (client.isConnected()) {
                     client.disconnect();
-                    System.out.println("Client disconnected from broker.");
+                    logger.info("Client disconnected from broker.");
                 }
             } catch (MqttException e) {
-                System.err.println("Error while disconnecting:");
+                logger.error("Error while disconnecting:" + e.getMessage());
                 e.printStackTrace();
             }
         }
-
     }
-
-    public static void main(String[] args) {
-        PublishingServiceClientImpl publishing = new PublishingServiceClientImpl();
-        try {
-            String message = "<event>\n" +
-            "<string key=\"concept:name\" value=\"Next event\"/>\n" +
-            "<date key=\"time:timestamp\" value=\"2024-09-11T15:56:16.000+00:00\"/>\n" +
-            "<string key=\"perform:donor\" value=\"D001\"/>\n" +
-            "<string key=\"location:station\" value=\"Left station\"/>\n" +
-            "</event>";
-            //message = publishing.prepareMessage();
-            String jsonMessage = "{"
-                    + "\"concept:name\": \"EJUB\", "
-                    + "\"time:timestamp\": \"2024-09-11T15:56:16.000+00:00\", "
-                    + "\"perform:donor\": \"D001\", "
-                    + "\"location:station\": \"Left station\""
-                    + "}";
-            publishing.publishMessage(message, "test/topic");
-            //publishing.publishMessage(message);
-        } catch (MqttException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
